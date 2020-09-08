db073adb6f970f3f35070ef83a1fb949fc1c5f4a
unional <homawong@gmail.com>
2020-09-08 00:34:05 -0700

    chore: init commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'roots': ['src'],
+  'watchPlugins': [
+    'jest-watch-suspend',
+    'jest-watch-repeat',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config', { 'setting': 'verbose' },
+    ],
+    [
+      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+    ],
+  ],
+}
