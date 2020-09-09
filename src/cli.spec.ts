7515b7ca1c239fac5e7e45611473acd392317472
unional <homawong@gmail.com>
2020-09-09 01:00:03 -0700

    chore: update tsconfig setting
    update deps

--- src/cli.spec.ts
+++ src/cli.spec.ts
@@ -1,1 +1,16 @@
-test('empty', () => {})
+import { createCliArgv } from 'clibuilder'
+import { cli } from './cli'
+import { CLI_NAME } from './pkg'
+
+describe('sensible defaults', () => {
+  test.skip('todo', async () => {
+    await cli.parse(createCliArgv(CLI_NAME))
+  })
+})
+
+// test('--show-config', async () => {
+//   const { argv, ui } = testCli(cli, ['--show-config'])
+
+//   await cli.parse(argv)
+//   generateDisplayedMessage(ui.infoLogs)
+// })
